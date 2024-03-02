46a736d3f098e668955b305b0dbcc07df0ad75d6
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-02 13:10:41 -0800

    Create chromaDB.py
    vector database - Chroma

---
+++ vectordatabase/chromaDB.py
@@ -0,0 +1,25 @@
+import os
+import chromadb
+from chromadb.config import Settings
+
+
+base_directory = r"C:\Users\Solmaz"
+
+
+relative_directory = ".cache"
+
+persist_directory = os.path.join(base_directory, relative_directory)
+
+Create the ChromaDB client with the updated persist directory
+#chroma_client = chromadb.Client(
+    Settings(
+        chroma_db_impl="duckdb+parquet",
+        persist_directory="/content/.."
+    )
+)
+
+collection.add(
+    documents=pdf_subset["title"][:100].tolist(),
+    metadatas=[{"topic": topic} for topic in pdf_subset["topic"][:100].tolist()],
+    ids=[f"id{x}" for x in range(100)],
+)
