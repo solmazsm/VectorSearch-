58a9af1818da0f6e6969a8e3c7465b002761045f
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 10:40:46 -0800

    Update queries.py

--- EfficientRetrieval/queries.py
+++ EfficientRetrieval/queries.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title: multi-vector search - queries
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 queries = ["animal", "space", "science"]
 search_results = multi_vector_search(queries, index_content, pdf_to_index)
 for idx, query_result in enumerate(search_results):
