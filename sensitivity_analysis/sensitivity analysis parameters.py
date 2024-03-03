66b8fffb331a90279c2600ea349960e717086629
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:10:19 -0800

    Update sensitivity analysis parameters.py

--- sensitivity_analysis/sensitivity analysis parameters.py
+++ sensitivity_analysis/sensitivity analysis parameters.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title:  sensitivity_analysis
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 index_dimensions = [256, 512, 1024]
 similarity_thresholds = [0.7, 0.8, 0.9]
 pretrained_models = ["all-MiniLM-L6-v2", "roberta-base", "bert-base-uncased"]
