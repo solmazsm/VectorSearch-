1d578b73679d7576a845020a0e6c3f0a3ddf7c82
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:22:57 -0800

    Update search_similar_documents_hnswlib.py
    search_similar_documents_hnswlib

--- similarity search/HNSWlib version/search_similar_documents_hnswlib.py
+++ similarity search/HNSWlib version/search_similar_documents_hnswlib.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title:  search_similar_documents_hnswlib
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 def search_similar_documents_hnswlib(query_text, index, pdf_subset, k=3):
     query_vector = model.encode([query_text])
     labels, distances = index.knn_query(query_vector, k=k)
