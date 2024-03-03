ce8905f8960aff0f1dd77408cd15a4e301646cde
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:20:00 -0800

    Update query.py

--- hnswlib/query.py
+++ hnswlib/query.py
@@ -1,2 +1,8 @@
+;==========================================
+; Title:  title_embeddings
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 query = "science"
 similar_docs = search_similar_documents(query, index, pdf_subset)
