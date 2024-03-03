0a0518db4301d38a65f87b44cd26de6f99e58729
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:47:37 -0800

    Update main.py
    Main function

--- evaluate_performance_for_queries/Evaluate performance for the set of queries/main.py
+++ evaluate_performance_for_queries/Evaluate performance for the set of queries/main.py
@@ -1,1 +1,25 @@
 
+def main():
+      
+    # Create SentenceTransformer model
+    cache_dir = r"C:\Users\Solmaz\.cache"
+    model = SentenceTransformer("all-MiniLM-L6-v2", cache_folder=cache_dir)
+    
+    # Encode 
+    title_embeddings = encode_document_titles(pdf_subset, model)
+    
+    # HNSWlib index
+    index_hnswlib = create_hnswlib_index(title_embeddings)
+    
+    #FAISS index
+    index_faiss = create_faiss_index(title_embeddings)
+    
+    #queries
+    evaluation_queries = ["science", "technology", "health", "environment", "business", "politics", "sports", "entertainment"]
+    
+   
+    results_hnswlib, results_faiss = evaluate_performance_for_queries(evaluation_queries, index_hnswlib, index_faiss, model, pdf_subset)
+    
+    
+if __name__ == "__main__":
+    main()
