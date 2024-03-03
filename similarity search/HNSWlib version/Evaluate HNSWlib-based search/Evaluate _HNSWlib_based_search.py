d62619edc06750e27a1288f6c17187072088b0bb
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:30:30 -0800

    Create Evaluate _HNSWlib_based_search.py

---
+++ similarity search/HNSWlib version/Evaluate HNSWlib-based search/Evaluate _HNSWlib_based_search.py
@@ -0,0 +1,4 @@
+for query, relevant_ids in ground_truth.items():
+    similar_docs_hnswlib = search_similar_documents_hnswlib(query, index_hnswlib, pdf_subset)
+    precision, recall = evaluate_performance(relevant_ids, similar_docs_hnswlib)
+    hnswlib_results.append({'query': query, 'precision': precision, 'recall': recall})
