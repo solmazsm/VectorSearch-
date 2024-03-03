47f9c932ba85836ba318477b9247a28b0fa62da3
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:09:33 -0800

    Update queries a collection.py
    collection.query

--- queries/queries a collection.py
+++ queries/queries a collection.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title: collection.query
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 import json
 
 results = collection.query(query_texts=["space"], n_results=10)
