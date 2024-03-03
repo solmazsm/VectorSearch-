a601372d728bd9fe873deb3bee00c3b3c1830233
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:18:32 -0800

    Update search_similar_documents.py

--- hnswlib/search_similar_documents.py
+++ hnswlib/search_similar_documents.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title:  title_embeddings
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 def search_similar_documents(query_text, index, pdf_subset, k=3):
     query_vector = model.encode([query_text])
     labels, distances = index.knn_query(query_vector, k=k)
