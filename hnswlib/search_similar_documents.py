34b383b7b7840f3c17d01508be59a52d6b0eeefc
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:18:01 -0800

    Create search_similar_documents.py

---
+++ hnswlib/search_similar_documents.py
@@ -0,0 +1,6 @@
+def search_similar_documents(query_text, index, pdf_subset, k=3):
+    query_vector = model.encode([query_text])
+    labels, distances = index.knn_query(query_vector, k=k)
+    similar_documents = pdf_subset.iloc[labels[0]].reset_index(drop=True)
+    similar_documents['distance'] = distances[0]
+    return similar_documents
