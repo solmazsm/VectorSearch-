ccedba9a54a83c758a44e87d64d0f701b4833188
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 11:30:48 -0800

    Update Evaluate _HNSWlib_based_search.py

--- similarity search/HNSWlib version/Evaluate HNSWlib-based search/Evaluate _HNSWlib_based_search.py
+++ similarity search/HNSWlib version/Evaluate HNSWlib-based search/Evaluate _HNSWlib_based_search.py
@@ -1,3 +1,9 @@
+;==========================================
+; Title:  search_similar_documents_hnswlib
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
 for query, relevant_ids in ground_truth.items():
     similar_docs_hnswlib = search_similar_documents_hnswlib(query, index_hnswlib, pdf_subset)
     precision, recall = evaluate_performance(relevant_ids, similar_docs_hnswlib)
