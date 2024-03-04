029b7c16eefbac8c9335ec59b61166df6031d1e5
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 22:45:29 -0800

    Create multi-vector_search.py

---
+++ Vectorse/Doc2Vec model/multi-vector_search.py
@@ -0,0 +1,20 @@
+# Function to perform multi-vector search
+def multi_vector_search(queries, index, pdf_to_index, k=3):
+    results = []
+    for query_text in queries:
+        # Encode the query using the model
+        query_vector = model.encode(query_text)
+        if len(query_vector.shape) == 1:
+            query_vector = query_vector.reshape(1, -1)
+        faiss.normalize_L2(query_vector)
+
+        # Perform similarity search
+        top_k = index.search(query_vector, k)
+
+        # Retrieve and process results
+        ids = top_k[1][0].tolist()
+        similarities = top_k[0][0].tolist()
+        docs = pdf_to_index.loc[ids]
+        docs["similarities"] = similarities
+        results.append(docs)
+    return results
