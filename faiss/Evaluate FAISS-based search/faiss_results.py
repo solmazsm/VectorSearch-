7c8a7197238b25611851d9d3fe010c7c95d77185
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 23:06:08 -0800

    Update faiss_results.py

--- faiss/Evaluate FAISS-based search/faiss_results.py
+++ faiss/Evaluate FAISS-based search/faiss_results.py
@@ -1,3 +1,16 @@
+;==========================================
+; Title: Evaluate FAISS-based search
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;==========================================
+
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+import faiss
+import numpy as np
+
+
 faiss_results = []
 for query, relevant_ids in ground_truth.items():
     similar_docs_faiss = search_similar_documents_faiss(query, index_faiss, pdf_subset)
