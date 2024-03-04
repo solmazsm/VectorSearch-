ac6cae8d4bdb97f2db2239101e4c360a8461d880
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-03 23:09:45 -0800

    Update efficient_similarity_search_using _FAISS.py

--- faiss/Evaluate FAISS-based search/efficient similarity search using FAISS/efficient_similarity_search_using _FAISS.py
+++ faiss/Evaluate FAISS-based search/efficient similarity search using FAISS/efficient_similarity_search_using _FAISS.py
@@ -1,2 +1,12 @@
+;==========================================
+; Title: efficient similarity search using FAISS
+; Author: Solmaz Seyed Monir
+; Date:   3 March 2024
+;========================================== 
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
 index_faiss = faiss.IndexFlatIP(dim)
 index_faiss.add(title_embeddings)
